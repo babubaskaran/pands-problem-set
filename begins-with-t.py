732d7bfd79d0319bf51a334f6cfbe90e1fe24ae4
Babu Baskaran <babubaskaran@gmail.com>
2019-04-05 19:32:23 +0100

    solution 1 of problem set finished

--- begins-with-t.py
+++ begins-with-t.py
@@ -1,5 +1,5 @@
 # Author : Babu Baskaran 
-# Date   : 05/04/2019 Time : 17:00 pm
+# Date   : 05/04/2019 Time : 19:00 pm
 # Solution for problem number 1
 user1=int (input("Please Enter a Positive Integer : "))
 
@@ -7,7 +7,7 @@ start = user1
 
 i = 1
 
-ans = 0
+ans = 1
 while i<= start:
     ans = ans+i
     i = i + 1
