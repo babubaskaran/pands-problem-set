b8413013992c99b57d33a8acefbecde0cf4692d6
Babu Baskaran <babubaskaran@gmail.com>
2019-04-06 21:52:16 +0100

    checked divided by 2

--- collatz.py
+++ collatz.py
@@ -2,3 +2,21 @@
 # Date   : 06/04/2019 Time : 21:00 pm
 # Solution for problem number 4
 # Version 1.0
+user1=int (input("Please Enter a Positive Integer : "))
+
+start = user1 % 2
+
+if start  == 0: 
+    print(user1)
+
+else:
+  print("not even")
+
+#i = 1
+
+#ans = 0
+#while i>= 1:
+ #   ans = ans+i
+  #  i = i + 1
+    
+#print(ans)
