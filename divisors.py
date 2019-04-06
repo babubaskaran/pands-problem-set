12be327c8434d232f180e72fe779efc652636254
Babu Baskaran <babubaskaran@gmail.com>
2019-04-06 15:47:01 +0100

    problem 3 saved

---
+++ divisors.py
@@ -0,0 +1,15 @@
+# Author : Babu Baskaran 
+# Date   : 06/04/2019 Time : 13:00 pm
+# Solution for problem number 3
+# Version 1.0
+
+number = [1000, 1001, 1002]
+
+for i in number:
+    div = i / 6
+print ("the divided num is", i, "is", div)
+ #if  (div == 0):
+  # print(divs)
+ #else: 
+    
+print("have a nice day")
