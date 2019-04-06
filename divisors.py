114fba34c56a25e4d4916b99e31b2951039c76a7
Babu Baskaran <babubaskaran@gmail.com>
2019-04-06 21:22:14 +0100

    problem 3 done divisable 12 not excluded

--- divisors.py
+++ divisors.py
@@ -3,17 +3,11 @@
 # Solution for problem number 3
 # Version 1.0
 
-for i in range(1000, 1010):
-  div = i / 6
-
- # start = 10000
-#ans = 0
-
-#if i<= start:
-    #ans = ans+i
-    #i = i - 1
-  print ( i)
-
-
-#else:
-print("have a nice day")
+for i in range(1000, 10000):
+  for i2 in range(1000, 10000):
+    div1 = i / 6
+    div2 = i /12
+    res2 = div2 % 2
+    res1 = div1 % 2
+  if (res2 != 0) and   (res1 == 0): 
+    print (i)
