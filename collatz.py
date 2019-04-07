b8a2c6b5607af39c3753cdc94bd6ec68770b43e7
Babu Baskaran <babubaskaran@gmail.com>
2019-04-07 20:50:42 +0100

    solution 4 collatz done

--- collatz.py
+++ collatz.py
@@ -1,17 +1,27 @@
 # Author : Babu Baskaran 
-# Date   : 06/04/2019 Time : 21:00 pm
+# Date   : 07/04/2019 Time : 17:00 pm
 # Solution for problem number 4
 # Version 1.0
-user1=int (input("Please Enter a Positive Integer : "))
 
+# define the function col to perform the repeated steps based on condition
 def col(user1):
-  while user1 !=1:
-
+    # if condition to check the entered value is divide by 2 print it
     if user1 % 2==0:
-      user1 = (user1/2)
-      return print (int(user1))
+     #print the result 
+     print (int(user1/2))
+     # return to value of function 
+     return user1/2
+    # elif condition to check next value is odd 
     elif user1 % 2==1:
-      user1 = (3*user1+1)
-      return print (int(user1))  
-      
-col(user1)
+      # perform  multiply by 3 and add one  
+      user2 = (3*user1+1)
+      # print the result
+      print (int(user2))
+      #  return to value of function
+      return user2
+# getting user input 
+n = input("Please Enter a Positive Integer : ")
+# while condition to check next value is not equal to 1
+while n !=1:
+  # asssigning value of n from the function
+  n = col(int(n))
