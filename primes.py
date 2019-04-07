2f1a7966824eb136d478ac5cddea1f5534725ac2
Babu Baskaran <babubaskaran@gmail.com>
2019-04-07 22:03:02 +0100

    prime number solution done

--- primes.py
+++ primes.py
@@ -2,18 +2,18 @@
 # Date   : 07/04/2019 Time : 20:00 pm
 # Solution for problem number 5
 # Version 1.0
-user1=int (input("Please Enter a Positive Integer : "))
-# assigning user input into a variable
-start = user1
-# set i start value into 1
-i = 1
-# set value of variable ans to zero
-ans = 0
-# check the i value is equal or less than variable start
-while i<= start:
-    # increase the value of variable ans by 1
-    ans = ans+i
-    # increase the value of i by 1
-    i = i + 1
-# print the output sum of integer to the user    
-print(ans)
+inputData=input("Please Enter a Positive Integer : ")
+inputNum=int(inputData)
+
+if inputNum > 1:
+    for i in range(2,inputNum):
+        if (inputNum % i) == 0:
+            print("This is not Prime")
+            break
+    else:
+        print("This is a Prime")
+        
+
+else:
+    print ("This is not Prime")
+
