754e67d0a3458299fe484bc75f1ec123828d886a
Babu Baskaran <babubaskaran@gmail.com>
2019-04-07 23:08:44 +0100

    solution for problem 8

--- datetime.py
+++ datetime.py
@@ -7,18 +7,6 @@
 
 import time 
   
-b = time.strftime("%A, %B %d %Y at %I:%M%p")
+b = time.strftime("%A, %B %dth %Y at %I:%M%p")
 print(b)
 
-# checking the weekday number is equal to 1 using if condition
-# if datetime.datetime.today().weekday() == 1: 
-#     # print result if the weekday number is 1 
-#     print("Yes - today begins with a T")
-# # checking the weekday number is equal to 3  using elif conditon  
-# elif datetime.datetime.today().weekday() == 3: 
-#   # print result if the weekday number is 3   
-#   print("Yes - today begins with a T")
-# # pass on if the weekday number is not 1 or 3  
-# else:
-#   # print the day is not beging with T since the weekday is not 1 & 3
-#   print("No - today is doesn't begins with T")
